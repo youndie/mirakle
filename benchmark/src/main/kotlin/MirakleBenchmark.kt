768c5a9016261968b120596986d4b7f6727046fb
adambl4 <dreadambl4@gmail.com>
2018-03-07 13:57:11 +0300

    Refactor benchmark

--- benchmark/src/main/kotlin/MirakleBenchmark.kt
+++ benchmark/src/main/kotlin/MirakleBenchmark.kt
@@ -1,3 +1,8 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.internal.AbstractTask
+import org.gradle.api.invocation.Gradle
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.ProjectConnection
 import org.gradle.tooling.events.ProgressListener
@@ -11,72 +16,126 @@ import java.io.OutputStream
 import java.io.PrintWriter
 import java.util.*
 
-val benchmarkSubject = File("${System.getProperty("user.home")}/work/benchmark_subject")
-val benchmarkName = "with_stub"
-val launchNumber = 5
-val tasks = listOf("clean", "assembleDevDebug")
+class MirakleBenchmark : Plugin<Gradle> {
+    override fun apply(gradle: Gradle) {
+        gradle.rootProject { it.extensions.create("mirakleBenchmark", MirakleBenchmarkExtension::class.java) }
 
-val gradleOutputFile = File("benchmark_results/${benchmarkName}_gradle_output.txt")
-val resultsFile = File("benchmark_results/${benchmarkName}_result.txt")
+        if (gradle.startParameter.projectProperties.containsKey(BENCHMARK)) return
+        if (!gradle.startParameter.taskNames.contains("mirakleBenchmark")) return
 
-val printerWriter get() = PrintWriter(FileOutputStream(resultsFile, true))
-
-fun main(args: Array<String>) {
-    File("benchmark_results").apply {
-        if(!exists()) mkdir()
-    }
-
-    if(gradleOutputFile.exists()) {
-        gradleOutputFile.delete()
-    }
-
-    gradleOutputFile.createNewFile()
-
-    if(resultsFile.exists()) {
-        resultsFile.delete()
-    }
-    resultsFile.createNewFile()
-
-    printerWriter.use { logOut ->
-        logOut.apply {
-            appendln("Project dir   = $benchmarkSubject")
-            appendln("Launch number = $launchNumber")
-            appendln("Tasks         = $tasks")
-            appendln("Name          = $benchmarkName")
-            appendln("Date          = ${Date()}")
-            appendln()
+        val originalStartParams = gradle.startParameter.newInstance()
+        originalStartParams.apply {
+            setTaskNames(taskNames.minus("mirakleBenchmark"))
         }
-    }
 
-    val connection = GradleConnector.newConnector().forProjectDirectory(benchmarkSubject).connect()
+        gradle.startParameter.apply {
+            setTaskNames(listOf("mirakleBenchmark"))
+            setExcludedTaskNames(listOf("mirakle"))
+            useEmptySettings()
 
-    try {
-        (1..launchNumber).asSequence().map {
-            println("Launching № $it...")
-            runTasks(*tasks.toTypedArray(), connection = connection, output = FileOutputStream(gradleOutputFile, true))
-        }.forEachIndexed { i, (beforeFirstTask, upload, execute, download, total) ->
+            buildFile = File(originalStartParams.currentDir, "mirakle.gradle").takeIf(File::exists)
+                    ?: //a way to make Gradle not evaluate project's default build.gradle file on local machine
+                    File(originalStartParams.currentDir, "mirakle_build_file_stub").also { stub ->
+                        stub.createNewFile()
+                        gradle.rootProject { it.afterEvaluate { stub.delete() } }
+                    }
+        }
 
-            fun format(long: Long) = "${long / 1000.0} secs"
+        gradle.rootProject { project ->
+            project.afterEvaluate {
+                val benchmarkConfig = project.extensions.getByType(MirakleBenchmarkExtension::class.java)
+                val mirakleConfig = project.extensions.getByName("mirakle")
 
-            printerWriter.use { logOut ->
-                logOut.apply {
-                    appendln("----- LAUNCH № ${i + 1} -----")
-                    appendln("Init     : ${format(beforeFirstTask)}")
-                    appendln("Upload   : ${format(upload)}")
-                    appendln("Execute  : ${format(execute)}")
-                    appendln("Download : ${format(download)}")
-                    appendln("Total    : ${format(total)}")
-                    appendln("-----------------------")
-                    appendln()
+                if (benchmarkConfig.name == null) throw IllegalArgumentException("Mirakle benchmark name is not defined.")
+                if (benchmarkConfig.launchNumber == null) throw IllegalArgumentException("Mirakle benchmark launch number is not defined.")
+
+                project.task<AbstractTask>("mirakleBenchmark") {
+                    doFirst {
+                        val resultsRoot = File("build/mirakle-benchmark-results/${benchmarkConfig.name}")
+
+                        resultsRoot.apply {
+                            if (exists()) deleteRecursively()
+                            mkdirs()
+                        }
+
+                        val gradleOutputFile = File("$resultsRoot/gradle_output.txt")
+                        val resultsFile = File("$resultsRoot/result.txt")
+
+                        gradleOutputFile.createNewFile()
+                        resultsFile.createNewFile()
+
+                        fun printerWriter() = PrintWriter(FileOutputStream(resultsFile, true))
+
+                        val mirakleConfigClass = mirakleConfig::class.java
+
+                        StringBuilder().apply {
+                            appendln("Name                  = ${benchmarkConfig.name}")
+                            appendln("Project dir           = ${project.rootDir}")
+                            appendln("Tasks                 = ${originalStartParams.taskNames}")
+                            appendln("Launch number         = ${benchmarkConfig.launchNumber}")
+                            appendln("Date                  = ${Date()}")
+                            appendln("-----")
+                            appendln("Host                  = ${mirakleConfigClass.getMethod("getHost").invoke(mirakleConfig)}")
+                            appendln("Rsync to remote       = ${mirakleConfigClass.getMethod("getRsyncToRemoteArgs").invoke(mirakleConfig)}")
+                            appendln("Rsync from remote     = ${mirakleConfigClass.getMethod("getRsyncFromRemoteArgs").invoke(mirakleConfig)}")
+                            appendln("Download in parallel  = ${mirakleConfigClass.getMethod("getDownloadInParallel").invoke(mirakleConfig)}")
+                            appendln("Download interval     = ${mirakleConfigClass.getMethod("getDownloadInterval").invoke(mirakleConfig)}")
+                            appendln()
+                        }.let {
+                            printerWriter().use { logOut ->
+                                logOut.append(it)
+                            }
+
+                            print(it)
+                        }
+
+                        val connection = GradleConnector.newConnector()
+                                .forProjectDirectory(gradle.rootProject.projectDir)
+                                .connect()
+
+                        try {
+                            (1..benchmarkConfig.launchNumber!!).asSequence().map {
+                                println("Launching № $it...")
+                                runTasks(originalStartParams.taskNames, connection, FileOutputStream(gradleOutputFile, true))
+                            }.forEachIndexed { i, (beforeFirstTask, upload, execute, download, total) ->
+
+                                fun format(long: Long) = "${long / 1000.0} secs"
+
+                                StringBuilder().apply {
+                                    appendln("----- LAUNCH № ${i + 1} -----")
+                                    appendln("Init     : ${format(beforeFirstTask)}")
+                                    appendln("Upload   : ${format(upload)}")
+                                    appendln("Execute  : ${format(execute)}")
+                                    appendln("Download : ${format(download)}")
+                                    appendln("Total    : ${format(total)}")
+                                    appendln("-----------------------")
+                                    appendln()
+                                }.let {
+                                    printerWriter().use { logOut ->
+                                        logOut.append(it)
+                                    }
+
+                                    print(it)
+                                }
+                            }
+                        } finally {
+                            connection.close()
+                        }
+                    }
                 }
             }
         }
-    } finally {
-        connection.close()
     }
 }
 
-fun runTasks(vararg tasks: String, connection: ProjectConnection, output: OutputStream): LogEntry {
+const val BENCHMARK = "mirakle.benchmark"
+
+open class MirakleBenchmarkExtension {
+    var name: String? = null
+    var launchNumber: Int? = null
+}
+
+fun runTasks(tasks: List<String>, connection: ProjectConnection, output: OutputStream): LogEntry {
     var buildStarted: Long = 0
     var buildFinished: Long = 0
 
@@ -92,7 +151,8 @@ fun runTasks(vararg tasks: String, connection: ProjectConnection, output: Output
     var downloadFinished: Long = 0
 
     connection.newBuild()
-            .forTasks(*tasks)
+            .forTasks(*tasks.toTypedArray())
+            .withArguments("-P$BENCHMARK=true")
             .setStandardOutput(output)
             .setStandardError(output)
             .addProgressListener(ProgressListener { event ->
@@ -142,3 +202,7 @@ data class LogEntry(val beforeFirstTask: Long,
                     val execute: Long,
                     val download: Long,
                     val total: Long)
+
+
+inline fun <reified T : Task> Project.task(name: String, noinline configuration: T.() -> Unit) =
+        tasks.create(name, T::class.java, configuration)
